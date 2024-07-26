220f1dcef5bdbfef9fad05c6f01a72df7af3f167
charles-david <charlesdavidguehi.pro@gmail.com>
2024-07-26 13:36:24 +0200

    first commit

---
+++ src/dtos/attribut-objet-metier-body.dto.ts
@@ -0,0 +1,68 @@
+import { IsString, IsDefined, IsOptional } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger"
+import { Expose, Type } from "class-transformer"
+
+@Expose()
+export class AttributObjetMetierBodyDto {
+
+    @ApiProperty({
+        type: String,
+        example: 'nomClient'    ,
+        description: "La donnée représentée par cet attribut",
+    })
+    @IsString()
+    @IsDefined()
+    donnee: string
+
+    @ApiProperty({
+        type: String,
+        example: '@MaxLength(10)',
+        description: "Contraintes spécifiques appliquées à l'attribut",
+    })
+    @IsString()
+    @IsOptional()
+    contrainte?: string
+
+    @ApiProperty({
+        type: String,
+        example: 'String'    ,
+        description: "Type de donnée de l'attribut",
+    })
+    @IsString()
+    @IsDefined()
+    type: string
+
+    @ApiProperty({
+        type: String,
+        example: "Description de l'attribut" })
+        @IsString()
+            @IsOptional()
+        description?: string
+
+        @ApiProperty({
+            type: String,
+            example: '"Exemple de valeur"'    ,
+            description: "Exemple de valeur pour l'attribut",
+        })
+            @IsString()
+            @IsOptional()
+        exemple?: string
+
+        @ApiProperty({
+            type: String,
+            example: 'Attribut Initial' ,
+            description: "Nom initial de l'attribut dans le modèle de données",
+        })
+            @IsString()
+            @IsDefined()
+        nomInitial: string
+
+        @ApiProperty({
+            type: GenreBodyDto,
+            example: 'masculin'    ,
+            description: "Genre de l'attribut (masculin, féminin, etc.)",
+    })
+    @IsDefined()
+    genre: Genre
+
+}
---
+++ src/dtos/attribut-objet-metier.response.dto.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { Expose } from "class-transformer"
+import {IsDefined} from "class-validator";
+
+@Expose()
+export class AttributObjetMetierResponseDto {
+
+    @ApiProperty({
+        type: String,
+        example: "nomClient"    ,
+        description: "La donnée représentée par cet attribut",
+    })
+    @IsDefined()
+    donnee: string;
+
+    @ApiProperty({
+        type: String,
+        example: "@MaxLength(10)"    ,
+        description: "Contraintes spécifiques appliquées à l'attribut",
+    })
+    contrainte?: string;
+
+    @ApiProperty({
+        type: String,
+        example: "String"    ,
+        description: "Type de donnée de l'attribut",
+    })
+    @IsDefined()
+    type: string;
+
+    @ApiProperty({
+        type: String,
+        example: "Description de l'attribut"  })
+    description?: string;
+
+    @ApiProperty({
+        type: String,
+        example: "Exemple de valeur",
+        description: "Exemple de valeur pour l'attribut",
+    })
+    exemple?: string;
+
+    @ApiProperty({
+        type: String,
+        example: "Attribut Initial"    ,
+        description: "Nom initial de l'attribut dans le modèle de données",
+    })
+    @IsDefined()
+    nomInitial: string;
+
+    @ApiProperty({
+        type: GenreResponseDto,
+        example: "masculin"    ,
+        description: "Genre de l'attribut (masculin, féminin, etc.)",
+    })
+    @IsDefined()
+    genre: Genre;
+
+}
